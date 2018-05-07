61257109dc596b3af255bba0fb039b0810938b5f
Glaucia Lemos <glaucia.lemos@esx.com.br>
2018-05-07 19:20:45 -0300

    commit

--- src/parte-1/olaMundo.js
+++ src/parte-1/olaMundo.js
@@ -11,13 +11,13 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-//Configuração do Server via Restify:
+// Configuração do Server via Restify:
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function() {
     console.log('%s Aplicação está executando na porta %s', server.name, server.url);
 });
 
-//Criação do chat connector para comunicar com o serviço do Bot Framework:
+// Criação do chat connector para comunicar com o serviço do Bot Framework:
 var connector = new builder.ChatConnector({
     appId:'',
     appPassword: ''
@@ -26,7 +26,7 @@ var connector = new builder.ChatConnector({
 //Endpoint para executar as mensagens para os usuários via Bot Emulator:
 server.post("/api/messages", connector.listen());
 
-//Aqui entra os nossos diálogos:
+// Aqui entra os nossos diálogos:
 var bot = new builder.UniversalBot(connector, function(session) {
     session.send("Você disse: %s", session.message.text);
 });

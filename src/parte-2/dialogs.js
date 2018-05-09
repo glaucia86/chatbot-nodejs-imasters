2fd31842bf3b6d48dd95b49f85e33049f89335d7
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-05-09 18:38:13 -0300

    Finalização do exemplo de 'dialogs' artigo iMasters.

--- src/parte-2/dialogs.js
+++ src/parte-2/dialogs.js
@@ -29,5 +29,34 @@ const bot = new builder.UniversalBot(connector);
 
 // Bloco de Diálogos:
 bot.dialog('/', [
-    
+    session => {
+        builder.Prompts.text(session, "Oi! Tudo bem? Como você se chama?");
+    },
+
+    (session, results) => {
+        let nome = results.response;
+        session.send(`Oi! ${nome}`);
+    }
+]);
+
+// Novo Bloco de Diálogos:
+bot.dialog('/perguntarPratoPredileto', [
+    session => {
+        builder.Prompts.text(session, "Qual é o seu prato predileto?");
+    },
+
+    (session, results) => {
+        let pratoPredileto = results.response;
+        builder.Prompts.text(`Puxa que legal! Então você gosta de comer **${pratoPredileto}**!`);
+    }
+]);
+
+bot.dialog('/lugarPredileto', [
+    session => {
+        builder.Prompts.text(session, 'E qual é o seu lugar preferido?');
+    },
+    (session, results) => {
+            let lugarPreferido = results.response;
+            builder.Prompts.text(`Amamos o **${lugarPredileto}**! Realmente é simplesmente muito lindo!`)
+    }
 ]);

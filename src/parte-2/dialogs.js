571ff23d32e51083aba83c9e6a5f6d5d1a6a45f4
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-05-09 18:21:57 -0300

    Alteração no exemplo do post do imasters.

--- src/parte-2/dialogs.js
+++ src/parte-2/dialogs.js
@@ -6,3 +6,28 @@
  * Author: Glaucia Lemos
  *
  */
+
+const restify = require("restify");
+const builder = require("botbuilder");
+
+// Configuração do Server via Restify:
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log("%s Aplicação executando na porta %s", server.name, server.url);
+});
+
+// Criação do chat connector para comunicar com o serviço do Bot Framework:
+const connector = new builder.ChatConnector({
+  appId: "",
+  appPassword: ""
+});
+
+// Endpoint para executar as mensagens para os usuários:
+server.post("api/messages", connector.listen());
+
+const bot = new builder.UniversalBot(connector);
+
+// Bloco de Diálogos:
+bot.dialog('/', [
+    
+]);

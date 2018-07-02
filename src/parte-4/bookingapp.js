3be3e95e165b44bc206eb8959467e9ce198cb6b0
Glaucia Lemos <glaucia86>
2018-07-02 16:45:03 -0300

    Desenvolvimento da aplicação bot com LUIS.

--- src/parte-4/bookingapp.js
+++ src/parte-4/bookingapp.js
@@ -9,3 +9,26 @@
 
 // Aqui estou carregando os enviroments que estão vindo do
 // arquivo 'env':
+require('dotenv-extended').load({
+    path: '../.env'
+});
+
+const builder = require('botbuilder');
+const restify = require('restify');
+
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log('%s Aplicação executando na porta %s', server.name, server.url);
+});
+
+//===> Configuração do Bot:
+let connector = new builder.ChatConnector({
+    appId: "",
+    appPassword: ""
+});
+
+//===> Configuração LUIS:
+let recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+let intents = new builder.IntentDialog({ recognizers: [recognizer] });
+
+let bot = new builder.UniversalBot(connector);

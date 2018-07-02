1291639b861b8f7ba52ecc2c51b854bee0c1675c
Glaucia Lemos <glaucia86>
2018-07-02 17:31:24 -0300

    Conclusão do desenv.

--- src/parte-4/bookingapp.js
+++ src/parte-4/bookingapp.js
@@ -32,3 +32,26 @@ let recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 let intents = new builder.IntentDialog({ recognizers: [recognizer] });
 
 let bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+bot.dialog('/', intents);
+intents.matches('None', '/none')
+    .matches('ObterTemperatura', '/obterTemperatura')
+    .matches('ReservarViagem', '/reservarViagem')
+    .onDefault(builder.DialogAction.send("Desculpe. Mas, não compreendi o que você quis dizer."))
+
+//=======> Bloco de Diálogos do Bot
+
+bot.dialog('/none', (session) => {
+    session.send("Não há intenção!");
+});
+
+bot.dialog('/obterTemperatura', (session) => {
+    //Add custom code here to implent get weather feature
+    session.send("Intenção de Tempo");
+});
+
+bot.dialog('/reservarViagem', (session) => {
+    //Add custom code here to implent book flight feature
+    session.send("Intenção de Reserva de Viagem.");
+});

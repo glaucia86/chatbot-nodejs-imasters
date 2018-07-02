721ba8564bf76f07a5526bdeadf9f689382c7f2e
Glaucia Lemos <glaucia86>
2018-07-02 14:51:07 -0300

    Inclusão do pacote 'dotenv-extended' na aplicação.

---
+++ src/parte-4/bookingapp.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * Arquivo: bookingapp.js
+ * Data: 02/07/2018
+ * Descrição: Desenvolvimento de um Bot de pedido de pizza integrado com o LUIS.
+ * Author: Glaucia Lemos
+ *
+ */
+
+// Aqui estou carregando os enviroments que estão vindo do
+// arquivo 'env':
